6936e44ace90d09e616f0ae17617b30288e60576
yakir <yakirifrah19@gmail.com>
2019-06-17 14:08:53 +0300

    fix Ex3 start oop week 4

--- Week 5/javascript OOP 2/start/Ex3/script.js
+++ Week 5/javascript OOP 2/start/Ex3/script.js
@@ -1,32 +1,34 @@
 class Account {
-    constructor (sum) {
+    constructor(sum) {
         this.sum = sum;
     }
-    deposit (value) {
+    deposit(value) {
         this.sum += value;
     }
-    withdraw (value){
-        this.sum -= value;
+    withdraw(value) {
+        if (this.sum - value > 0) {
+            this.sum -= value;
+        }
     }
-    canWithdraw (value) {
+    canWithdraw(value) {
         return this.sum - value > 0;
     }
     lottery(account) {
-        let ranNumber = Math.floor(Math.random() * 100); 
+        let ranNumber = Math.floor(Math.random() * 100);
         console.log(ranNumber);
         if (!account.canWithdraw(100)) {
             return `you didnt withdraw 100$`;
         }
-        if (ranNumber %1 === 0) {
+        if (ranNumber % 1 === 0) {
             account.deposit(30);
         }
         this.withdraw(account.sum);
     }
 }
-let account = new Account (1000);
+let account = new Account(1000);
 for (let index = 0; index < 5; index++) {
-   let canWithdraw100 = account.lottery(account);
-   console.log(canWithdraw100);
+    let canWithdraw100 = account.lottery(account);
+    console.log(canWithdraw100);
 }
-console.log("final sum: "+account.sum);
+console.log("final sum: " + account.sum);
 
