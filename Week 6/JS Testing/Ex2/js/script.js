536671b8a70923e887413710f49aea38030a1ff1
yakir <yakirifrah19@gmail.com>
2019-06-19 13:33:51 +0300

    added some classes

--- Week 6/JS Testing/Ex2/js/script.js
+++ Week 6/JS Testing/Ex2/js/script.js
@@ -1,6 +1,7 @@
 function transpose(array2d) {
     return array2d[0].map((col, i) => array2d.map(row => row[i]));
 }
+
+
+// let array2d = [[1,2],[3,4,5]];
 module.exports = transpose;
-let result = transpose([[1, 2, 3],
-[4, 5]]);
