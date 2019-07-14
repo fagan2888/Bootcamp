d23007014ddbf733b7a13cb321bbe58468340523
yakir <yakirifrah19@gmail.com>
2019-07-14 12:13:15 +0300

    added ex2

---
+++ Week 10/Python Env/start/Ex2/main.py
@@ -0,0 +1,6 @@
+from moda import oprint_name
+from modb import print_nickname
+
+if __name__ == "__main__":
+    print(oprint_name("yakir"))
+    print(print_nickname("yak"))
---
+++ Week 10/Python Env/start/Ex2/modb.py
@@ -0,0 +1,2 @@
+def print_nickname(nickname):
+    return nickname
