7a78275032a58100ea2a9506b8bd5f6d36789625
yakir <yakirifrah19@gmail.com>
2019-07-14 11:46:12 +0300

    ex1 enve py

---
+++ Week 10/Python Env/start/Ex1/ex1.py
@@ -0,0 +1,23 @@
+#Q1
+import sys
+print(sys.builtin_module_names)
+
+#Q2
+import _abc
+import _ast
+import _bisect
+
+#Q3
+from math import sin
+
+#Q4
+from math import sqrt, sin, cos
+
+#Q5
+from _random import *
+
+from math import factorial as calcFactorial,exp as exponent,ceil as ceiling
+
+assert(calcFactorial(5) == 120)
+print(exponent(3))
+print(ceiling(3.2))
