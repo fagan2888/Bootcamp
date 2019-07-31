c097a2e07db643c8b32b9fd3c2247bf830e5c29e
yakir <yakirifrah19@gmail.com>
2019-07-31 15:14:46 +0300

    finish guided

--- Exercises/Week 12 - bottle & django/Intro to Django/Guided/test-project-django/mysite/mysite/settings.py
+++ Exercises/Week 12 - bottle & django/Intro to Django/Guided/test-project-django/mysite/mysite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pizza_store'
+    'polls.apps.PollsConfig'
 ]
 
 MIDDLEWARE = [
--- Exercises/Week 12 - bottle & django/Intro to Django/Guided/test-project-django/mysite/pizza_store/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PizzaStoreConfig(AppConfig):
-    name = 'pizza_store'
