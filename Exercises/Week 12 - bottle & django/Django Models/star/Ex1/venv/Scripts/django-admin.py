fe402de5563dbbabb338d0e9a4c22882ee8b92d2
yakir <yakirifrah19@gmail.com>
2019-07-31 18:04:29 +0300

    ex1

---
+++ Exercises/Week 12 - bottle & django/Django Models/star/Ex1/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\dev\bootcamp\bootcamp\exercises\week 12 - bottle & django\django models\star\ex1\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
